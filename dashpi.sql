8fa660a84341385db4c25c5adc069ae85948a9af
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-03-29 22:04:54 +0300

    add config, set removed files status to removed

--- dashpi.sql
+++ dashpi.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.1.1 on Tue Mar 28 15:27:52 2017
+-- File generated with SQLiteStudio v3.1.1 on Wed Mar 29 13:07:48 2017
 --
 -- Text encoding used: UTF-8
 --
@@ -8,11 +8,32 @@ BEGIN TRANSACTION;
 
 -- Table: files
 DROP TABLE IF EXISTS files;
-CREATE TABLE files (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, session VARCHAR (64) NOT NULL REFERENCES sessions (session), path VARCHAR (128) NOT NULL, status INTEGER NOT NULL, created DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP));
+
+CREATE TABLE files (
+    id      INTEGER       PRIMARY KEY AUTOINCREMENT
+                          NOT NULL,
+    session VARCHAR (64)  NOT NULL
+                          REFERENCES sessions (session),
+    path    VARCHAR (128) NOT NULL
+                          UNIQUE ON CONFLICT ROLLBACK,
+    status  INTEGER       NOT NULL,
+    created DATETIME      NOT NULL
+                          DEFAULT (CURRENT_TIMESTAMP) 
+);
+
 
 -- Table: sessions
 DROP TABLE IF EXISTS sessions;
-CREATE TABLE sessions (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, session VARCHAR (64) NOT NULL, created DATETIME CONSTRAINT "CURRENT_TIMESTAMP" DEFAULT (CURRENT_TIMESTAMP) NOT NULL);
+
+CREATE TABLE sessions (
+    id      INTEGER      PRIMARY KEY AUTOINCREMENT
+                         UNIQUE
+                         NOT NULL,
+    session VARCHAR (64) NOT NULL,
+    created DATETIME     CONSTRAINT [CURRENT_TIMESTAMP] DEFAULT (CURRENT_TIMESTAMP) 
+                         NOT NULL
+);
+
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
