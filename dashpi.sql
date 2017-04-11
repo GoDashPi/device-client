87e20c63286ea3742c722f002bcb4b12d9376862
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-04-11 09:07:12 +0300

    change path column attributes in db

--- dashpi.sql
+++ dashpi.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.1.1 on Mon Apr 10 09:36:14 2017
+-- File generated with SQLiteStudio v3.1.1 on Tue Apr 11 09:06:08 2017
 --
 -- Text encoding used: UTF-8
 --
@@ -7,10 +7,33 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: files
-CREATE TABLE files (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, session VARCHAR (64) NOT NULL REFERENCES sessions (session), path VARCHAR (128) NOT NULL UNIQUE ON CONFLICT ROLLBACK, status INTEGER NOT NULL, created DATETIME NOT NULL);
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE files (
+    id      INTEGER       PRIMARY KEY AUTOINCREMENT
+                          NOT NULL
+                          UNIQUE,
+    session VARCHAR (64)  NOT NULL
+                          REFERENCES sessions (session),
+    path    VARCHAR (128) NOT NULL,
+    status  INTEGER       NOT NULL,
+    created DATETIME      NOT NULL
+);
+
 
 -- Table: sessions
-CREATE TABLE sessions (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, session VARCHAR (64) NOT NULL, created DATETIME CONSTRAINT "CURRENT_TIMESTAMP" DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')) NOT NULL, record_start DATETIME);
+DROP TABLE IF EXISTS sessions;
+
+CREATE TABLE sessions (
+    id           INTEGER      PRIMARY KEY AUTOINCREMENT
+                              UNIQUE
+                              NOT NULL,
+    session      VARCHAR (64) NOT NULL,
+    created      DATETIME     CONSTRAINT [CURRENT_TIMESTAMP] DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW') ) 
+                              NOT NULL,
+    record_start DATETIME
+);
+
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
