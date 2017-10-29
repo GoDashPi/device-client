63d5694c6169affff665e0f6cf93696a1343d6b8
Eetu Tuomala <eetu.tuomala@sc5.io>
2017-10-29 10:00:48 +0200

    add Sequelize and refactor database

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const apiKey = fs.readFileSync('./.apikey', { encoding: 'utf8' });
 process.env.API_KEY = apiKey.replace('\n', '');
 process.env.DASHPI_ENV = process.env.DASHPI_ENV || 'device';
 
-log(`STARTING DASH PI [${process.env.DASHPI_ENV}]`);
+log(`STARTING DASH PI [${process.env.DASHPI_ENV}] ${process.env.MODE}`);
 log(`API_KEY [${process.env.API_KEY}]`);
 
 const start = () =>
@@ -35,31 +35,36 @@ const upload = () =>
   uploader.upload()
     .then((status) => log('uploaded', status));
 
-controller.run().subscribe(cmd => {
-  log('exec', cmd);
-  switch (cmd) {
-    case 'start': {
-      start();
-      break;
+if (process.env.MODE === 'record') {
+  start();
+} else if (process.env.MODE === 'upload') {
+  upload();
+} else {
+  controller.run().subscribe(cmd => {
+    log('exec', cmd);
+    switch (cmd) {
+      case 'start': {
+        start();
+        break;
+      }
+      case 'stop': {
+        camera.stop();
+        gps.stop();
+        setTimeout(() => upload([
+          fileStatus.RECORDING,
+          fileStatus.READY_FOR_UPLOAD,
+          fileStatus.FAILED_TO_UPLOAD,
+        ]), 500);
+        break;
+      }
+      case 'upload': {
+        upload();
+        break;
+      }
+      default: {
+        log('invalid control', cmd);
+        break;
+      }
     }
-    case 'stop': {
-      camera.stop();
-      gps.stop();
-      setTimeout(() => upload([
-        fileStatus.RECORDING,
-        fileStatus.READY_FOR_UPLOAD,
-        fileStatus.FAILED_TO_UPLOAD,
-      ]),
-      500);
-      break;
-    }
-    case 'upload': {
-      upload();
-      break;
-    }
-    default: {
-      log('invalid control', cmd);
-      break;
-    }
-  }
-});
+  });
+}
--- scripts/start-development.sh
+++ scripts/start-development.sh
@@ -2,6 +2,10 @@
 
 # dev settings for Mac
 
+if [ -z "${MODE}" ]; then
+  export MODE=record
+fi
+
 export CAPTURE_PARAMS="-r 30 -s 1280x720 -f avfoundation -i default -c copy -map 0 -segment_time 3 -c:v libx264 -preset fast -crf 30 -f segment"
 #export API_KEY=$(cat .apikey | xargs)
 export DASHPI_API=https://9fdbmnfvt1.execute-api.eu-west-1.amazonaws.com/dev
